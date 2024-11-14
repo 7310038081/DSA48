683195c4411c1a2ede27f309580542e112a67cfb
Knight00 <vs292713@gmail.com>
2024-11-14 18:33:22 +0530

    Initial commit

---
+++ vd_01/program.cpp
@@ -0,0 +1,79 @@
+// // program to print the odd numbers 1 to n inclusive 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter the number:";
+//     cin>>n;
+
+//     int number= 1;
+
+//     while(number<=n){
+//         if(number%2!=0){
+//             cout<<number<<endl;
+//         }
+//         number = number +1;
+//     }
+
+// }
+
+// // program to print the even numbers 1 to n exclusive
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+
+//     int n;
+//     cout<<"Enter the number:";
+//     cin>>n;
+//     int number = 2;
+    
+//     while(number<n){
+//         cout<<number<<endl;
+//         number = number+2;
+//     }
+// }
+
+// program to find the sum from 1 to n inclusive
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n ; 
+//     cout<<"Enter the number:";
+//     cin>>n;
+
+//     int sum = 0;
+//     int number = 1;
+
+//     while(number<=n){
+//         sum = sum + number;
+//         number = number + 1;
+//     }
+//     cout<<"The sum is: "<<sum;
+// }
+
+// program to find the factorial of a given number
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+ 
+
+//     int product = 1;
+
+    
+//     for(int i =1; i<=n; i++){
+
+//         product = product*i;
+//     }
+
+//     cout<<product;
+    
+// }
