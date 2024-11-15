499518e88f894b853322ecf8750d6d6ed1fbceae
Knight00 <vs292713@gmail.com>
2024-11-15 11:23:23 +0530

    initial commit

---
+++ vd_02/program.cpp
@@ -0,0 +1,25 @@
+/*
+understanding the flow and about the program
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"Hello World"<<endl;
+}
+
+Datatypes and Variables
+int,float,double,char,bool
+
+how data is stored?
++ve numbers
+
+typecasting (conversion of one datatype into another datatype)
+int a = 'a'
+cout<<a<<endl;
+output: 97
+
+how -ve no is stored?
+how -ve no is printed?
+
+unsigned or signed 
+*/
